8d3cb0ae673d9823abfc36836fb0cf6cf7e7e258
rachelnova18 <rachelnv26@gmail.com>
2025-05-20 10:51:44 +0700

    add akses modifier

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -2,3 +2,10 @@
 using namespace std;
 
 class Node // membuat node
+{
+public:         // akses modifier
+    int data;   // data yang disimpan
+    Node *next; // pointer ke node berikutnya
+    // constructor
+
+    
